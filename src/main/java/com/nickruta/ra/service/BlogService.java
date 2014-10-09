cd339ac07d8e9cbbb3f94ae2d864c95d057fe093
Nick Ruta <nickruta@gmail.com>
2014-10-09 18:34:32 -0400

    Spring Security - pre authorize logic added for blog removal

--- src/main/java/com/nickruta/ra/service/BlogService.java
+++ src/main/java/com/nickruta/ra/service/BlogService.java
@@ -1,6 +1,8 @@
 package com.nickruta.ra.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.nickruta.ra.entity.Blog;
@@ -24,8 +26,13 @@ public class BlogService {
 		
 	}
 
-	public void delete(int id) {
-		blogRepository.delete(id);
+	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+	public void delete(@P("blog") Blog blog) {
+		blogRepository.delete(blog);
+	}
+
+	public Blog findOne(int id) {
+		return blogRepository.findOne(id);
 	}
 	
 	
