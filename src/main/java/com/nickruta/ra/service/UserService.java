b54d5b638e4e7beaaa1524bb74540d4b79828572
Nick Ruta <nickruta@gmail.com>
2014-10-11 12:01:29 -0400

    JQuery validation added for Unique Name field of Registration Ajax call added for UniqueName

--- src/main/java/com/nickruta/ra/service/UserService.java
+++ src/main/java/com/nickruta/ra/service/UserService.java
@@ -78,4 +78,9 @@ public class UserService {
 		public void delete(int id) {
 			userRepository.delete(id);			
 		}
+
+		public User findOne(String username) {
+			return userRepository.findByName(username);
+			
+		}
 }
